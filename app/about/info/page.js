656e63ec4e81251b92b11053f6ae40806e4293bd
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2024-10-23 02:22:40 +0100

    first commit

---
+++ app/about/info/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react'
+
+const AboutInfoPge = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>AboutInfoPge</h1>
+      <Link href='/' className='text-2xl'>
+        home page
+      </Link>
+    </div>
+  );
+};
+export default AboutInfoPge;
---
+++ app/client/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const ClientPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">ClientPage</h1>
+      <Link href="/about" className="text-2xl">
+        about page
+      </Link>
+    </div>
+  );
+};
+export default ClientPage;
